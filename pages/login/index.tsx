220ca818ea58ce9e548297babad8c952c260519d
dxe58709 <nsakanou@student.42tokyo.jp>
2024-02-27 14:41:56 +0000

    update

---
+++ pages/login/index.tsx
@@ -0,0 +1,45 @@
+import nextSession from "next-session";
+import type { GetServerSideProps } from "next";
+
+const getSession = nextSession({ autoCommit: false });
+
+export const getServerSideProps: GetServerSideProps = async ({ req, res }) => {
+  var session = await getSession(req, res);
+  if (Object.keys(session.data).includes("userId")) {
+    return {
+      redirect: {
+        destination: "/attendance",
+        permanent: true,
+      },
+    };
+  }
+  return {
+    props: {},
+  };
+};
+
+export default function Login() {
+  return (
+    <div>
+      <form action="/api/login" method="post">
+        <input
+          className="block"
+          type="text"
+          name="username"
+          id="username"
+          placeholder="username"
+        />
+        <input
+          className="block"
+          type="password"
+          name="password"
+          id="password"
+          placeholder="password"
+        />
+        <button className="block" type="submit">
+          login
+        </button>
+      </form>
+    </div>
+  );
+}
